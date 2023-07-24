f357d24acc0cd20c33879f5a5c109e6924133b61
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-24 20:58:19 -0300

    Feat: Using styles and images

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 class App extends Component{
   render(){
+    let nome = 'Castello';
+    let img = 'https://sujeitoprogramador.com/steve.png';
     return(
       <View>
         <Text>Olá Mundo!</Text>
+        <Text style={{color: 'red', fontSize: 25, margin: 15}}>Sujeito Programador</Text>
+        <Image
+          source={{uri: img}}
+          style={{width: 300, height: 300}}
+        />
+        <Text style={{fontSize:30, color: 'fuchsia'}}>{nome}</Text>
       </View>
     )
   }
