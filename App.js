53cf13007b3c78b96dcd2e64eef482027fbdefc7
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-24 21:07:33 -0300

    Feat: Using componentsw

--- App.js
+++ App.js
@@ -4,18 +4,30 @@ import { View, Text, Image } from 'react-native';
 class App extends Component{
   render(){
     let nome = 'Castello';
-    let img = 'https://sujeitoprogramador.com/steve.png';
     return(
       <View>
         <Text>Olá Mundo!</Text>
-        <Text style={{color: 'red', fontSize: 25, margin: 15}}>Sujeito Programador</Text>
-        <Image
-          source={{uri: img}}
-          style={{width: 300, height: 300}}
-        />
+        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>Sujeito Programador</Text>
+        <Jobs l={500} a={100} imgLabel={'Steve Jobs'}/>
         <Text style={{fontSize:30, color: 'fuchsia'}}>{nome}</Text>
       </View>
     )
+  }
+}
+
+class Jobs extends Component{
+  render(){
+    let img = 'https://sujeitoprogramador.com/steve.png';
+    
+    return(
+      <View>
+        <Image 
+          source={{ uri: img }}
+          style={{ width: this.props.l, height: this.props.a}}
+        />
+        <Text style={{fontSize: 35, color: 'green'}}>{this.props.imgLabel}</Text>
+      </View>
+    );
   }
 }
 
